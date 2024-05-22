dc70130810da629f8ce2500ad43e681330f89313
Umair Saeed <umair.saeed@gmail.com>
2024-05-21 19:43:47 -0700

    [02_Basic Calculator] 06 While loop

--- exercises/02_basic_calculator/06_while/src/lib.rs
+++ exercises/02_basic_calculator/06_while/src/lib.rs
@@ -4,7 +4,15 @@ pub fn factorial(n: u32) -> u32 {
     // interprets as "I'll get back to this later", thus
     // suppressing type errors.
     // It panics at runtime.
-    todo!()
+    let mut fact = 1;
+    let mut n = n;
+
+    while n > 0 {
+        fact *= n;
+        n = n - 1;
+    }
+
+    return fact;
 }
 
 #[cfg(test)]
