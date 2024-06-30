43d92a9350ebbba4b289e2d25f6dbe2bb8258787
Umair Saeed <umair.saeed@gmail.com>
2024-06-30 16:39:36 -0700

    [04_Traits] 13 Outro

--- exercises/04_traits/13_outro/src/lib.rs
+++ exercises/04_traits/13_outro/src/lib.rs
@@ -8,3 +8,92 @@
 //   It should be possible to print its debug representation.
 //
 // Tests are located in the `tests` folder—pay attention to the visibility of your types and methods.
+
+use std::ops::Add;
+
+#[derive(Debug, PartialEq, Copy, Clone)]
+pub struct SaturatingU16 {
+    value: u16,
+}
+
+impl SaturatingU16 {
+    fn new(value: u16) -> Self {
+        SaturatingU16 { value }
+    }
+}
+
+impl PartialEq<u16> for SaturatingU16 {
+    fn eq(&self, other: &u16) -> bool {
+        self.value == *other
+    }
+}
+
+// MARK: - From
+
+impl From<u16> for SaturatingU16 {
+    fn from(value: u16) -> Self {
+        SaturatingU16::new(value)
+    }
+}
+
+impl From<&u16> for SaturatingU16 {
+    fn from(value: &u16) -> Self {
+        SaturatingU16::new(*value)
+    }
+}
+
+impl From<u8> for SaturatingU16 {
+    fn from(value: u8) -> Self {
+        SaturatingU16::new(value.into())
+    }
+}
+
+impl From<&u8> for SaturatingU16 {
+    fn from(value: &u8) -> Self {
+        SaturatingU16::new((*value).into())
+    }
+}
+
+// MARK: - Add
+
+impl SaturatingU16 {
+    fn overflow_add(lhs: &u16, rhs: &u16) -> SaturatingU16 {
+        let result = match (*lhs).checked_add(*rhs) {
+            None => u16::MAX,
+            Some(v) => v,
+        };
+        SaturatingU16::new(result)
+    }
+}
+
+impl Add<u16> for SaturatingU16 {
+    type Output = Self;
+
+    fn add(self, rhs: u16) -> Self {
+        SaturatingU16::overflow_add(&self.value, &rhs)
+    }
+}
+
+impl Add<&u16> for SaturatingU16 {
+    type Output = SaturatingU16;
+
+    fn add(self, rhs: &u16) -> Self {
+        SaturatingU16::overflow_add(&self.value, rhs)
+    }
+}
+
+impl Add<SaturatingU16> for SaturatingU16 {
+    type Output = SaturatingU16;
+
+    fn add(self, rhs: SaturatingU16) -> Self {
+        SaturatingU16::overflow_add(&self.value, &rhs.value)
+    }
+}
+
+impl Add<&SaturatingU16> for SaturatingU16 {
+    type Output = SaturatingU16;
+
+    fn add(self, rhs: &SaturatingU16) -> Self {
+        SaturatingU16::overflow_add(&self.value, &(*rhs).value)
+    }
+}
