624ebb006d2e3695fdcac49c48194bc6ba40f582
Umair Saeed <umair.saeed@gmail.com>
2024-07-04 15:47:43 -0700

    [05_Tickets_v2] 07 Unwrap.

--- exercises/05_ticket_v2/07_unwrap/src/lib.rs
+++ exercises/05_ticket_v2/07_unwrap/src/lib.rs
@@ -1,8 +1,19 @@
+use std::thread::panicking;
+
 // TODO: `easy_ticket` should panic when the title is invalid.
 //   When the description is invalid, instead, it should use a default description:
 //   "Description not provided".
 fn easy_ticket(title: String, description: String, status: Status) -> Ticket {
-    todo!()
+    match Ticket::new(title.clone(), description.clone(), status.clone()) {
+        Ok(ticket) => ticket,
+        Err(error) => {
+            if error.contains("Description") {
+                Ticket::new(title, "Description not provided".to_string(), status).unwrap()
+            } else {
+                panic!("{error}");
+            }
+        }
+    }
 }
 
 #[derive(Debug, PartialEq, Clone)]
@@ -39,6 +50,10 @@ impl Ticket {
             description,
             status,
         })
+    }
+
+    fn is_description_valid(description: String) -> bool {
+        return description.is_empty() || description.len() > 500;
     }
 }
 
