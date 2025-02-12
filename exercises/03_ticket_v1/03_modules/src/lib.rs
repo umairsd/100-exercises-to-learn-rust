2ddcbf34ce49ed3a0a6ac92750e0190d181fb818
Umair Saeed <umair.saeed@gmail.com>
2025-02-12 06:35:13 -0800

    [03_Ticket] 03 (Modules), 04 (Visibility), 05 (Encapsulation)

--- exercises/03_ticket_v1/03_modules/src/lib.rs
+++ exercises/03_ticket_v1/03_modules/src/lib.rs
@@ -1,9 +1,12 @@
 mod helpers {
+    // Option 1:
+    // use crate::Ticket;
+
     // TODO: Make this code compile, either by adding a `use` statement or by using
     //  the appropriate path to refer to the `Ticket` struct.
 
-    fn create_todo_ticket(title: String, description: String) -> Ticket {
-        Ticket::new(title, description, "To-Do".into())
+    fn create_todo_ticket(title: String, description: String) -> crate::Ticket {
+        crate::Ticket::new(title, description, "To-Do".into())
     }
 }
 
